3a18b92e16ed097a0c1c92c3cb2953afaa386603
Yohan Estiven Daza Rojas <yohangr4@gmail.com>
2022-10-19 15:39:25 -0500

    ejercicio 1

---
+++ Ejercicio1/src/com/example1/Ejercicio1.java
@@ -0,0 +1,43 @@
+package com.example1;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+
+        //numericos
+
+        //1.1 enteros
+        byte variable1 = 5;
+        System.out.println(variable1);
+        short variable2 = 10;
+        System.out.println(variable2);
+        int variable3 = 30;
+        System.out.println(variable3);
+        long variable4 = 100;
+        System.out.println(variable4);
+
+        //1.2 decimales
+
+        float variable5 = 5.5f;
+        System.out.println(variable5);
+        double variable6 = 10.5d;
+        System.out.println(variable6);
+
+
+        //booleano
+
+        boolean variable7 = false;
+        System.out.println(variable7);
+        boolean variable8 = true;
+        System.out.println(variable8);
+
+
+        //texto
+
+        char variable9 = 'a';
+        System.out.println(variable9);
+        String variable10 = "Cualquier texto";
+        System.out.println(variable10);
+    }
+}
